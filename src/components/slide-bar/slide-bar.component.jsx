024b807b337e330fb2e0c30954822ce9f35f8c0b
wedhimd <wedhimd@gmail.com>
2021-04-18 14:58:43 +0200

    modified index page and add logo and favicon

---
+++ src/components/slide-bar/slide-bar.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./slide-bar.styles.scss";
+
+class SlideBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SlideBar;
---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SlideBar from "../../components/slide-bar/slide-bar.component";
+import "./home-page.styles.scss";
+
+const HomePage = () => (
+  <div className="home-page">
+    <SlideBar />
+  </div>
+);
+
+export default HomePage;
